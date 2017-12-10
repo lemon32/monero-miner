e33580d2d79f6af6923bcc908745efe4e1f43004
GloriousYellow <yellowglorious@gmail.com>
2017-12-10 17:39:29 -0500

    fix: update libmicrohttpd path

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ LD_RUN_PATH=lib make
 
 mkdir lib
 cp /usr/lib/x86_64-linux-gnu/libuv.so.1 lib
-cp /usr/lib/x86_64-linux-gnu/libmicrohttpd.so.12 lib
+cp /usr/lib/x86_64-linux-gnu/libmicrohttpd.so lib
 
 rm ../.gitignore || true
