4f894e9e127c0cb9f8706df66612093158ade84c
GloriousYellow <yellowglorious@gmail.com>
2017-12-13 16:02:34 -0500

    fix: bundle libgcrypt

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ cmake ..
 LD_RUN_PATH=lib make
 
 mkdir lib
-cp /usr/lib/x86_64-linux-gnu/lib{uv,gnutls,curl,microhttpd}*.so* lib
+cp /usr/lib/x86_64-linux-gnu/lib{uv,gnutls,curl,microhttpd,gcrypt}*.so* lib
 
 rm ../.gitignore || true
