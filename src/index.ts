acb7b2db2907bcfbc3cea5a0deaa257302972ad0
GloriousYellow <yellowglorious@gmail.com>
2017-12-16 11:52:19 -0500

    feat: additionalArgs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import { dirname, resolve } from 'path'
 
 export const minerExecutablePath = resolve(__dirname, '../xmrig/build/xmrig')
 
-export function startMiner(pool: string, username: string, password?: string) {
+export function startMiner(
+  pool: string,
+  username: string,
+  password?: string,
+  additionalArgs?: string[],
+) {
   const args = [
     '-o',
     pool,
@@ -13,6 +18,7 @@ export function startMiner(pool: string, username: string, password?: string) {
     '--donate-level=1',
     '--print-time=10',
     '--max-cpu-usage=95',
+    ...(additionalArgs || []),
   ]
   if (password) {
     args.push('-p', password)
